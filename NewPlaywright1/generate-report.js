94e65c43debba1a10561cf4b69702c499f209b1b
Manu-Test28 <manusweetiee@gmail.com>
2025-06-15 07:22:40 -0700

    Update generate-report.js

--- NewPlaywright1/generate-report.js
+++ NewPlaywright1/generate-report.js
@@ -1,11 +1,25 @@
-const reporter = require('cucumber-html-reporter');
+const reporter = require('multiple-cucumber-html-reporter');
 
-const options = {
-  theme: 'bootstrap',
-  jsonFile: 'playwright-report/report.json',
-  output: 'playwright-report/report.html',
-  reportSuiteAsScenarios: true,
-  launchReport: false
-};
-
-reporter.generate(options);
+reporter.generate({
+  jsonDir: 'playwright-report', // where report.json is saved
+  reportPath: 'playwright-report',
+  metadata:{
+    browser: {
+        name: 'chrome',
+        version: '114'
+    },
+    device: 'Local test machine',
+    platform: {
+        name: 'Windows',
+        version: '10'
+    }
+  },
+  customData: {
+    title: 'Run info',
+    data: [
+        {label: 'Project', value: 'Playwright + Cucumber'},
+        {label: 'Release', value: '1.0.0'},
+        {label: 'Execution Time', value: new Date().toLocaleString()},
+    ]
+  }
+});
