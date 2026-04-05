b44eebb3bf9d0cb0a3d050783f6cca11be17edf5
Manu-Test28 <manusweetiee@gmail.com>
2026-04-05 22:18:23 +0530

    Integrate Allure reporter into Cucumber configuration
    Added Allure reporter and results directory options.

--- NewPlaywright1/cucumber.js
+++ NewPlaywright1/cucumber.js
@@ -1,7 +1,13 @@
 module.exports = {
   default: {
     require: ['src/steps/**/*.ts', 'src/support/**/*.ts'],
-    format: ['@cucumber/pretty-formatter'],
+    format: [
+      '@cucumber/pretty-formatter',
+      'allure-cucumberjs/reporter'          // ← ADD THIS
+    ],
+    formatOptions: {
+      resultsDir: 'allure-results'           // ← ADD THIS
+    },
     paths: ['features/**/*.feature'],
     parallel: 1,
     publishQuiet: true,
